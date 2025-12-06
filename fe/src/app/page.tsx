5d063bbe2138bd6a6d8e5807be788ae6b93cbfce
lamlib <lamlib2023@gmail.com>
2025-12-07 00:02:58 +0700

    fix: remove dead code

--- fe/src/app/page.tsx
+++ fe/src/app/page.tsx
@@ -1,9 +1,6 @@
-import StandardPopover from "../components/ui/Popover";
-
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <StandardPopover></StandardPopover>
     </div>
   );
 }
