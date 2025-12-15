69f8a66fb9251c0f076341295dcced252456f7b0
lamlib <lamlib2023@gmail.com>
2025-12-15 20:42:52 +0700

    chore: remove trash

--- be/src/main/java/com/lamlib/learning/config/WebMvcConfig.java
+++ be/src/main/java/com/lamlib/learning/config/WebMvcConfig.java
@@ -2,7 +2,6 @@ package com.lamlib.learning.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
